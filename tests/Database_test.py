543f8ae2601eb3c1c82d0d81cd873354524ebfa4
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2020-02-25 15:27:46 +0100

    Trying waiting longer.

--- tests/Database_test.py
+++ tests/Database_test.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from ga4gh.wes.Database import Database
 from ga4gh.wes.RunStatus import RunStatus
 from testcontainers.mongodb import MongoDbContainer
@@ -8,6 +9,7 @@ from pymongo import MongoClient
 def database_connection():
     container = MongoDbContainer('mongo:4.2.3')
     container.start()
+    time.sleep(120)
     database = Database(MongoClient(container.get_connection_url()), "WES_Test")
     yield database
     database._db_runs().drop()
