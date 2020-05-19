e7a77fee576df8619b0cf21f384fd43a4446ace2
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2020-05-19 17:00:40 +0200

    Refactored create_app and main to follow 12-factor app principles and simplify development.

--- ga4gh/wes/wesnake.py
+++ ga4gh/wes/wesnake.py
@@ -15,8 +15,8 @@ from ga4gh.wes.ServiceInfo import ServiceInfo
 from ga4gh.wes.ErrorCodes import ErrorCodes
 import ga4gh.wes.routes
 
-def read_swagger():
-    '''Read the swagger file'''
+def create_connexion_app():
+    '''Read the swagger file.'''
     # This is hardcoded, because if it is changed, probably also quite some
     # code needs to be changed.
     swagger_file = "workflow_execution_service_1.0.0.yaml"
@@ -27,7 +27,7 @@ def read_swagger():
     with open(swagger_path, "r") as yaml_file:
         swagger = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
-    return swagger
+    return app, swagger
 
 
 def create_database(config):
@@ -36,12 +36,46 @@ def create_database(config):
                     "WES")
 
 
+def parse_cli(default_config, default_log_config, default_validation_config):
+    '''Command-line options: Take all values from there, defaulting to
+    ones from the environment or the static defaults. Note that if
+    default_config is None, then the parameter is required as CLI parameter.'''
+    parser = argparse.ArgumentParser(description="WESnake")
+
+    if default_config is not None:
+        parser.add_argument("--config", type=str, required=False,
+                            default=default_config)
+    else:
+        # When there is no environment variable, then a value is required.
+        parser.add_argument("--config", type=str, required=True)
+
+    # These remain optional as command-line parameters.
+    parser.add_argument("--log_config", type=str, required=False,
+                        default=default_log_config)
+    parser.add_argument("--validation", type=str, required=False,
+                        default=default_validation_config)
+
+    args = parser.parse_args()
+
+    return {
+        "config": args.config,
+        "log_config": args.log_config,
+        "validation": args.validation
+    }
+
+
 # This function takes parameters to simplify unit testing. Otherwise
 # environment or configuration value processing is done in this
 # function.
 #
 # All configuration-reading is located here to allow using `flask run`  with
-# the primari aim to simplify development in the conctainer
+# the primary aim to simplify development in the container. Note, however,
+# that this currently fails with
+#
+#  FLASK_APP="ga4gh/wes/wesnake:create_app()" flask run
+#
+# Because connexion does not return a flask app.
+#
 def create_app(config = None,
                validation = None,
                log_config = None,
@@ -57,58 +91,44 @@ def create_app(config = None,
     provided WESNAKE_LOG_CONFIG and WESNAKE_VALIDATION_CONFIG.
     '''
 
-    # Use the environment variables as default values.
-    default_log_config = os.getenv("WESNAKE_LOG_CONFIG",
-                                   os.path.join(sys.prefix, "config",
-                                                "log-config.yaml"))
-
-    default_validation_config = os.getenv("WESNAKE_VALIDATION_CONFIG",
-                                          os.path.join(sys.prefix, "config",
-                                                       "validation.yaml"))
-
     if config is None:
+        # Use the environment variables as default values.
+        default_config = os.getenv("WESNAKE_CONFIG", None)  # No static default here!
+        default_log_config = os.getenv("WESNAKE_LOG_CONFIG",
+                                       os.path.join(sys.prefix, "config",
+                                                    "log-config.yaml"))
+
+        default_validation_config = os.getenv("WESNAKE_VALIDATION_CONFIG",
+                                              os.path.join(sys.prefix, "config",
+                                                           "validation.yaml"))
         if "--config" in sys.argv:
-            # Command-line options: Take all values from there, defaulting to
-            # ones from the environment or the static defaults.
-            parser = argparse.ArgumentParser(description="WESnake")
-
-            if "WESNAKE_CONFIG" in os.environ:
-                parser.add_argument("--config", type=str, required=False,
-                                    default=os.environ.get("WESNAKE_CONFIG"))
-            else:
-                # When there is no environment variable, then a value is required.
-                parser.add_argument("--config", type=str, required=True)
-
-            # These remain optional as command-line parameters.
-            parser.add_argument("--log_config", type=str, required=False,
-                                default=default_log_config)
-            parser.add_argument("--validation", type=str, required=False,
-                                default=default_validation_config)
-
-            args = parser.parse_args()
+            args = parse_cli(default_config,
+                             default_log_config,
+                             default_validation_config)
         else:
             # No command-line options: Take all values from the environment.
             args = {
-                config: os.environ.get("WESNAKE_CONFIG"),
-                log_config: default_log_config,
-                validation: default_validation_config
+                "config": default_config,
+                "log_config": default_log_config,
+                "validation": default_validation_config
             }
 
-        with open(args.config, "r") as yaml_file:
-            config = yaml.load(yaml_file, Loader=yaml.FullLoader)
-
-        with open(args.validation, "r") as yaml_file:
-            validation = yaml.load(yaml_file, Loader=yaml.FullLoader)
-
-        with open(args.log_config, "r") as yaml_file:
+        with open(args["log_config"], "r") as yaml_file:
             log_config = yaml.load(yaml_file, Loader=yaml.FullLoader)
             dictConfig(log_config)
             logger = logging.getLogger("default")
+            logger.info("Read log config from " + args["log_config"])
+
+        with open(args["config"], "r") as yaml_file:
+            config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            logger.info("Read config from " + args["config"])
+
+        with open(args["validation"], "r") as yaml_file:
+            validation = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            logger.debug("Read validation specification from " + args["validation"])
 
         if database is None:
             database = create_database(config)
-
-    swagger = read_swagger()
 
     # Validate configuration YAML.
     validator = Validator()
@@ -117,6 +137,8 @@ def create_app(config = None,
         logger.error("Could not validate config.yaml: {}".
                      format(validator.errors))
         sys.exit(ErrorCodes.CONFIGURATION_ERROR)
+
+    app, swagger = create_connexion_app()
 
     # Use the conventional app.config attribute
     app.config = config
@@ -138,6 +160,7 @@ def create_app(config = None,
 
 
 def main():
+    print("Starting WESnake ...", file=sys.stderr)
 
     app = create_app()
 
