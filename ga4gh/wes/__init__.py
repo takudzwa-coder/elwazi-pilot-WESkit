d2ca23f1d1380973e96ecdebf9d4f938dc2dc344
Sven Twardziok <sven.twardziok@charite.de>
2020-07-09 14:22:32 +0200

    Adapt service name

--- ga4gh/wes/__init__.py
+++ ga4gh/wes/__init__.py
@@ -4,8 +4,8 @@ from celery import Celery
 def make_celery(app_name=__name__):
     celery = Celery(
         app="celery_app",
-        broker="redis://redis:6379",
-        backend="redis://redis:6379",
+        broker="redis://result_broker:6379",
+        backend="redis://result_broker:6379",
         include=["ga4gh.wes.tasks"])
     return(celery)
 
