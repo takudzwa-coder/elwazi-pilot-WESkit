aa656bed5c04def70c4e88fad938ab6f5df7585e
Sven Twardziok <sven.twardziok@charite.de>
2020-08-31 14:08:42 +0200

    worker vars to env

--- ga4gh/wes/celery_worker.py
+++ ga4gh/wes/celery_worker.py
@@ -1,7 +1,8 @@
 from ga4gh.wes import celery
+import os
 
 
 celery_config = dict()
-celery_config["broker_url"] = "redis://result_broker:6379"
-celery_config["result_backend"] = "redis://result_broker:6379"
+celery_config["broker_url"] = os.environ["BROKER_URL"]
+celery_config["result_backend"] = os.environ["RESULT_BACKEND"]
 celery.conf.update(celery_config)
