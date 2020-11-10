0a5b6a88c3e5e2a3b006fff1a301bcfc59f8188f
Sven Twardziok <sven.twardziok@posteo.de>
2020-11-10 17:49:22 +0100

    test disable pylint

--- ga4gh/wes/celery_worker.py
+++ ga4gh/wes/celery_worker.py
@@ -1,5 +1,5 @@
 from ga4gh.wes import celery
-from ga4gh.wes.tasks import run_snakemake
+from ga4gh.wes.tasks import run_snakemake # noqa: F401; pylint: disable=unused-variable
 import os
 
 
@@ -7,5 +7,6 @@ celery_config = dict()
 celery_config["broker_url"] = os.environ["BROKER_URL"]
 celery_config["result_backend"] = os.environ["RESULT_BACKEND"]
 celery.conf.update(celery_config)
-# registering task is not reqiured; just for fmt here
-celery.register_task(run_snakemake)
+# registering task is not required; just for fmt here
+# TODO remove statement and adapt gitlab pipeline
+# celery.register_task(run_snakemake)
