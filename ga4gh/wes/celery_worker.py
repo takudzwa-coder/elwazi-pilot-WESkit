d74182f5e8b96b4c8d0da4b2ae2616fc855caa3e
Sven Twardziok <sven.twardziok@posteo.de>
2020-11-10 18:04:37 +0100

    fmt

--- ga4gh/wes/celery_worker.py
+++ ga4gh/wes/celery_worker.py
@@ -7,6 +7,3 @@ celery_config = dict()
 celery_config["broker_url"] = os.environ["BROKER_URL"]
 celery_config["result_backend"] = os.environ["RESULT_BACKEND"]
 celery.conf.update(celery_config)
-# registering task is not required; just for fmt here
-# TODO remove statement and adapt gitlab pipeline
-# celery.register_task(run_snakemake)
