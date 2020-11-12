59a1e8199e661d12cca844e9b15aa986cb60d57a
Sven Twardziok <sven.twardziok@posteo.de>
2020-11-12 17:04:03 +0100

    remove

--- getapi.sh
+++
@@ -1,3 +0,0 @@
-APIURL="https://raw.githubusercontent.com/ga4gh/workflow-execution-service-schemas/master/openapi/workflow_execution_service.swagger.yaml"
-APIOUT="20191217_workflow_execution_service.swagger.yaml"
-wget $APIURL -O $APIOUT
