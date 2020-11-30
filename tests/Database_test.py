9dd95625a57da82a0235d066068e4d2f838f3902
Sven Twardziok <sven.twardziok@posteo.de>
2020-11-30 14:52:47 +0100

    remove manual id assignment

--- tests/Database_test.py
+++ tests/Database_test.py
@@ -1,48 +1,26 @@
 from wesnake.classes.RunStatus import RunStatus
 
-run_id = "test_store_and_retrieve_run_id"
-
-
-def test_create_new_run(database_connection):
+def test_create_and_load_run(database_connection):
     print("test create_new_run")
-    run = database_connection.create_new_run(run_id, [])
-    assert RunStatus.decode(run["run_status"]) == RunStatus.UNKNOWN
-    assert run["run_id"] == run_id
-
-
-def test_get_run(database_connection):
-    print("test get_run")
-    database_connection.create_new_run(run_id, [])
-    run = database_connection.get_run(run_id)
-    assert run["run_id"] == run_id
-
-
-def test_list_run_ids_and_states(database_connection):
-    print("test list_run_ids_and_states")
-    run_id_and_states = database_connection.list_run_ids_and_states()
-    assert len(run_id_and_states) == 0
-    database_connection.create_new_run(run_id, [])
+    run1 = database_connection.create_new_run([])
+    assert RunStatus.decode(run1["run_status"]) == RunStatus.UNKNOWN
+    run2 = database_connection.get_run(run1["run_id"])
+    assert run1 == run2
     run_id_and_states = database_connection.list_run_ids_and_states()
     assert len(run_id_and_states) == 1
-    assert run_id_and_states[0]["run_id"] == run_id
-    assert RunStatus.decode(run_id_and_states[0]["run_status"]) == \
-        RunStatus.UNKNOWN
-
 
 def test_update_run(database_connection):
     print("test update_run")
-    run = database_connection.create_new_run(run_id, [])
+    run = database_connection.create_new_run([])
     new_run = run.copy()
     new_run["run_status"] = RunStatus.RUNNING.encode()
     database_connection.update_run(new_run)
-    assert new_run["run_id"] == run_id
     assert RunStatus.decode(new_run["run_status"]) == RunStatus.RUNNING
     assert RunStatus.decode(run["run_status"]) == RunStatus.UNKNOWN
 
-
 def test_delete_run(database_connection):
     print("test delete_run")
-    database_connection.create_new_run(run_id, [])
-    database_connection.delete_run(run_id)
-    find_run = database_connection.get_run(run_id)
+    run = database_connection.create_new_run([])
+    database_connection.delete_run(run["run_id"])
+    find_run = database_connection.get_run(run["run_id"])
     assert find_run is None
