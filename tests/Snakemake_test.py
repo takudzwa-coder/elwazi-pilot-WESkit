8724d57624ec02ef6c4389944b90f7d6ab51c05c
Sven Twardziok <sven.twardziok@posteo.de>
2020-12-08 13:43:21 +0100

    add extended unit tests

--- tests/Snakemake_test.py
+++ tests/Snakemake_test.py
@@ -1,19 +1,70 @@
-import json, yaml, sys, time
+import json, yaml, sys, time, uuid, os
+from wesnake.classes.RunStatus import RunStatus
+from werkzeug.datastructures import FileStorage
+from werkzeug.datastructures import ImmutableMultiDict
+
+def get_pseudo_run(workflow_url):
+    run = {
+        "run_id": str(uuid.uuid4()),
+        "run_status": RunStatus.UNKNOWN.encode(),
+        "request_time": None,
+        "request": {
+            "workflow_url": workflow_url,
+            "workflow_params": '{"text":"hello_world"}'
+        },
+        "execution_path" : [],
+        "run_log": {},
+        "task_logs": [],
+        "outputs": {},
+        "celery_task_id": None,
+    }
+    return run
 
 
-def test_execute_snakemake_workflow(database_connection, snakemake, celery_worker, snakemake_wf1_data):
-    run = database_connection.create_new_run(request=snakemake_wf1_data)
-    snakemake.execute(run, database_connection)
+def test_prepare_execution(snakemake):
+    
+    # 1.) use workflow on server
+    run = get_pseudo_run(workflow_url=os.path.join(os.getcwd(), "tests/wf1/Snakefile"))
+    run = snakemake.prepare_execution(run, files=[])
+    assert run["run_status"] == RunStatus.INITIALIZING.encode()
+
+    # 2.) workflow does not exist on server -> error message to stderr file
+    run = get_pseudo_run(workflow_url=os.path.join(os.getcwd(), "tests/wf1/Filesnake"))
+    run = snakemake.prepare_execution(run, files=[])
+    assert run["run_status"] == RunStatus.SYSTEM_ERROR.encode()
+    assert os.path.isfile(run["run_log"]["stderr"])
+
+    # 3.) copy attached workflow to workdir
+    wf_url = "wf_1.smk"
+    with open(os.path.join(os.getcwd(), "tests/wf1/Snakefile"), "rb") as fp:
+        wf_file = FileStorage(fp, filename=wf_url)
+        files = ImmutableMultiDict({"workflow_attachment":[wf_file]})
+        run = get_pseudo_run(workflow_url=wf_url)
+        run = snakemake.prepare_execution(run, files)
+    assert run["run_status"] == RunStatus.INITIALIZING.encode()
+    assert os.path.isfile(os.path.join(run["execution_path"], wf_url))
+
+    # 4.) workflow is not attached -> error message to stderr file
+    run = get_pseudo_run(workflow_url="tests/wf1/Snakefile")
+    run = snakemake.prepare_execution(run, files=[])
+    assert run["run_status"] == RunStatus.SYSTEM_ERROR.encode()
+    assert os.path.isfile(run["run_log"]["stderr"])
+
+def test_execute_snakemake_workflow(snakemake, celery_worker):
+    run = get_pseudo_run(workflow_url=os.path.join(os.getcwd(), "tests/wf1/Snakefile"))
+    run = snakemake.prepare_execution(run, files=[])
+    run = snakemake.execute(run)
     running = True
     while running:
         time.sleep(1)
-        status = snakemake.get_state(run, database_connection)
-        print(status, file=sys.stderr)
+        status = snakemake.get_state(run)
         if (status == "COMPLETE"):
             running = False
+    assert run["run_status"] == RunStatus.COMPLETE.encode()
 
-def test_cancel_snakemake_workflow(database_connection, snakemake, celery_worker, snakemake_wf2_data):
-    run = database_connection.create_new_run(request=snakemake_wf2_data)
-    snakemake.execute(run, database_connection)
-    snakemake.cancel(run, database_connection)
-    assert snakemake.get_state(run, database_connection) == "CANCELED"
+def test_cancel_snakemake_workflow(snakemake, celery_worker):
+    run = get_pseudo_run(workflow_url=os.path.join(os.getcwd(), "tests/wf2/Snakefile"))
+    run = snakemake.prepare_execution(run, files=[])
+    run = snakemake.execute(run)
+    snakemake.cancel(run)
+    assert run["run_status"] == RunStatus.CANCELED.encode()
