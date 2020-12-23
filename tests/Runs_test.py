477123c58f930a6cdb3aa36c7ea6ac25627c1c9a
Sven Twardziok <sven.twardziok@posteo.de>
2020-12-23 18:37:52 +0100

    create new run class

---
+++ tests/Runs_test.py
@@ -0,0 +1,31 @@
+import sys, uuid
+from pymongo import MongoClient
+from wesnake.classes.Run import Run
+
+mock_run_data = {
+    "run_id": str(uuid.uuid4()),
+    "run_status": "UNKNOWN",
+    "request_time": None,
+    "request": {
+        "workflow_url": "",
+        "workflow_params": '{"text":"hello_world"}'
+    },
+}
+
+def test_create_and_load_run(database_container):
+    print(database_container.get_connection_url(), file=sys.stderr)
+    new_run = Run(mock_run_data)
+    new_run.set_status("RUNNING")
+    print(new_run, file=sys.stderr)
+    client = MongoClient(database_container.get_connection_url())
+    db = client["WES"]
+    collection = db["test_runs"]
+    collection.insert_one(new_run.get_data())
+    for x in collection.find():
+        load_run = Run(x)
+        print(load_run, file=sys.stderr)
+        print(type(load_run), file=sys.stderr)
+        print(load_run.get_status(), file=sys.stderr)
+
+    #database = Database(, "WES")
+    #database_container["run"].
---
+++ wesnake/classes/Run.py
@@ -0,0 +1,56 @@
+from wesnake.classes.RunStatus import RunStatus
+from typing import Generic
+
+class Run:
+    """ This is a Run."""
+
+    def __init__(self, data: dict) -> None:
+        # add validation here
+        self.run_id = data["run_id"]
+        self.run_status = data["run_status"]
+        self.request_time = data["request_time"]
+        self.request = data["request"]
+        
+        if "execution_path" in data.keys():
+            self.execution_path = data["execution_path"]
+        else:
+            self.execution_path = []
+        
+        if "run_log" in data.keys():
+            self.run_log = data["run_log"]
+        else:
+            self.run_log = {}
+        
+        if "task_logs" in data.keys():
+            self.task_logs = data["task_logs"]
+        else:
+            self.task_logs = []
+        
+        if "outputs" in data.keys():
+            self.outputs = data["outputs"]
+        else:
+            self.outputs = {}
+        
+        if "celery_task_id" in data.keys():
+            self.celery_task_id = data["celery_task_id"]
+        else:
+            self.celery_task_id = None
+    
+    def get_data(self) -> dict:
+        return {
+            "run_id": self.run_id,
+            "run_status": self.run_status,
+            "request_time": self.request_time,
+            "request": self.request,
+            "execution_path": self.execution_path,
+            "run_log": self.run_log,
+            "task_logs": self.task_logs,
+            "outputs": self.outputs,
+            "celery_task_id": self.celery_task_id,
+        }
+    
+    def set_status(self, status: str) -> None:
+        self.run_status = RunStatus[status].name
+    
+    def get_status(self) -> str:
+        return RunStatus[self.run_status].name
