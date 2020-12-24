6e1fbc8f75022e0976509846d0c8dcfa6d0f642b
Sven Twardziok <sven.twardziok@posteo.de>
2020-12-24 01:38:53 +0100

    formatting

--- wesnake/classes/Run.py
+++ wesnake/classes/Run.py
@@ -5,54 +5,58 @@ class Run:
     """ This is a Run."""
 
     def __init__(self, data: dict) -> None:
-        # add validation here; those values are required
+        # un
         self.__run_id = data["run_id"]
-        self.__run_status = data["run_status"]
         self.__request_time = data["request_time"]
         self.__request = data["request"]
 
+        if "celery_task_id" in data.keys():
+            self.celery_task_id = data["celery_task_id"]
+        else:
+            self.celery_task_id = None
+
         if "execution_path" in data.keys():
-            self.__execution_path = data["execution_path"]
+            self.execution_path = data["execution_path"]
         else:
-            self.__execution_path = []
-
-        if "run_log" in data.keys():
-            self.__run_log = data["run_log"]
-        else:
-            self.__run_log = {}
-
-        if "task_logs" in data.keys():
-            self.__task_logs = data["task_logs"]
-        else:
-            self.__task_logs = []
+            self.execution_path = []
 
         if "outputs" in data.keys():
-            self.__outputs = data["outputs"]
+            self.outputs = data["outputs"]
         else:
-            self.__outputs = {}
+            self.outputs = {}
 
-        if "celery_task_id" in data.keys():
-            self.__celery_task_id = data["celery_task_id"]
+        if "run_log" in data.keys():
+            self.run_log = data["run_log"]
         else:
-            self.__celery_task_id = None
+            self.run_log = {}
+
+        if "run_status" in data.keys():
+            self.run_status = data["run_status"]
+        else:
+            self.run_status = "UNKNOWN"
 
         if "start_time" in data.keys():
-            self.__start_time = data["start_time"]
+            self.start_time = data["start_time"]
         else:
-            self.__start_time = None
+            self.start_time = None
+
+        if "task_logs" in data.keys():
+            self.task_logs = data["task_logs"]
+        else:
+            self.task_logs = []
 
     def get_data(self) -> dict:
         return {
-            "celery_task_id": self.__celery_task_id,
-            "execution_path": self.__execution_path,
+            "celery_task_id": self.celery_task_id,
+            "execution_path": self.execution_path,
             "request": self.__request,
             "request_time": self.__request_time,
             "run_id": self.__run_id,
-            "run_status": self.__run_status,
-            "run_log": self.__run_log,
-            "outputs": self.__outputs,
-            "start_time": self.__start_time,
-            "task_logs": self.__task_logs
+            "run_log": self.run_log,
+            "run_status": self.run_status,
+            "outputs": self.outputs,
+            "start_time": self.start_time,
+            "task_logs": self.task_logs
         }
 
     @property
