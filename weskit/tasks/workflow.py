876595a27d447f43d0df325055bc82b1da297b65
jansa2708 <jan-sahrhage@gmx.de>
2021-01-28 08:36:29 +0100

    Fix testpipeline errors (E302, E501)

--- weskit/tasks/workflow.py
+++ weskit/tasks/workflow.py
@@ -3,12 +3,14 @@ import subprocess
 from weskit.tasks.celery import celery_app
 from snakemake import snakemake
 
+
 @celery_app.task(bind=True)
 def run_workflow(self, workflowfile, workdir, configfiles, **kwargs):
     outputs = []
 
     if pathlib.Path(workflowfile).suffix == ".nf":
-        subprocess.run(["nextflow", pathlib.PurePath(workflowfile).name], cwd=str(pathlib.PurePath(workflowfile).parent))
+        subprocess.run(["nextflow", pathlib.PurePath(workflowfile).name],
+                       cwd=str(pathlib.PurePath(workflowfile).parent))
     else:
         snakemake(
             snakefile=workflowfile,
