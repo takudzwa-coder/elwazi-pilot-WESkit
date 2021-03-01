7da37a040fa34410c019aaabb050a02174361cba
jansa2708 <jan-sahrhage@gmx.de>
2021-03-01 08:55:34 +0100

    Change celery_worker_pool back to prefork and add concurrency:10

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,7 +81,12 @@ def celery_config(redis_container):
 
 @pytest.fixture(scope="session")
 def celery_worker_pool():
-    return 'solo'
+    return 'prefork'
+
+
+@pytest.fixture(scope="session")
+def celery_worker_parameters():
+    return {"concurrency":10}
 
 
 @pytest.fixture(scope='session')
