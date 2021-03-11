f941a6285603076945b85d3f19f791c62c3f13c1
Ben Wulf <github@ben-wulf.de>
2021-03-11 10:07:45 +0000

    Add comments generateDevelopmentCerts.sh

--- uWSGI_Server/generateDevelopmentCerts.sh
+++ uWSGI_Server/generateDevelopmentCerts.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+# This script generates self signed development certificates.
+# Per default the certifcates will be stored in the certs subfolder.
+# The path can be changed by setting comand line arg 1
+# It will not overwrite existing certificates
+
+# localhost and hostname of the generating machine are valid dns names for the certifcate!
+
+
 
 if [ -z $1 ]; then 
-path=$(dirname $(realpath $pwd$0))"/certs"
+    # Set path to here/certs
+    certStoragePath=$(dirname $(realpath $pwd$0))"/certs"
 
-else path=$1;
+else
+    # Set path if commandline argument 1 is set
+    certStoragePath=$1;
 fi
-prefix=$path"/weskit"
+prefix=certStoragePath"/weskit"
 
 if [ ! -f $prefix".key" ] || [ ! -f $prefix".crt" ]; then
     echo "Least one certificate file missing / not found!"
-    echo "Generating new certificate and write it to: "$path".*"
+    echo "Generating new certificate and write it to: "certStoragePath".*"
     # Generate Self Signed Cert
     openssl req -x509 -newkey rsa:4096 -sha256 -days 30 -nodes -keyout $prefix.key -out $prefix.crt -subj "/CN=localhost" -addext "subjectAltName = DNS:localhost,DNS:127.0.0.1,DNS:"$(hostname) 
     # Convert Certificate to PEM Format needed for requests from python
