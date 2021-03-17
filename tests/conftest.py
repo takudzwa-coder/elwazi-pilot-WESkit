baf322d12d3857b1851d3d8ca13c6e825e97a47e
Ben Wulf <github@ben-wulf.de>
2021-03-17 13:04:44 +0100

    compose to basic containers

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,15 @@ import logging
 import os
 import pytest
 import yaml
+import requests
+import time
 from weskit.classes.Database import Database
 from weskit.classes.ServiceInfo import ServiceInfo
 from pymongo import MongoClient
 from testcontainers.mongodb import MongoDbContainer
 from testcontainers.redis import RedisContainer
-from testcontainers.compose import DockerCompose
+from testcontainers.mysql import MySqlContainer
+from testcontainers.core.container import DockerContainer
 from logging.config import dictConfig
 from weskit.classes.RunStatus import RunStatus
 
@@ -19,6 +22,7 @@ def get_redis_url(redis_container):
         redis_container.get_container_host_ip(),
         redis_container.get_exposed_port(6379)
     )
+    print("REDISURL:%s"%redis_container.get_container_host_ip())
     return url
 
 
@@ -26,9 +30,11 @@ def get_redis_url(redis_container):
 def test_app(database_container, redis_container,keycloak_container):
     os.environ["BROKER_URL"] = get_redis_url(redis_container)
     os.environ["RESULT_BACKEND"] = get_redis_url(redis_container)
+    
+    keycloakContainerProperties=getContainerProperties(keycloak_container,'8080')
     os.environ["kc_backend"]="http://%s:%s/auth/realms/WESkit" % (
-        keycloak_container.get_service_host("keycloak",8080),
-        keycloak_container.get_service_port("keycloak",8080),
+        keycloakContainerProperties["ExternalHostname"],
+        keycloakContainerProperties["ExposedPorts"],
     )
 
     # import here because env vars need to be set before
@@ -44,17 +50,67 @@ def test_app(database_container, redis_container,keycloak_container):
         ctx = app.app_context()
         ctx.push()
         yield testing_client
+        
+"""
+KeyCloak Containers
+
+"""
+
+def getContainerProperties(container,port):
+    return(
+        {
+        "ExternalHostname":container.get_container_host_ip(),
+        "InternalPorts":list(container.ports.keys()),
+        "ExposedPorts":container.get_exposed_port(port),
+        "InternalIP":container.get_docker_client().bridge_ip(container._container.id)
+        }
+    )
 
 @pytest.fixture(scope="session")
-def keycloak_container():
+def MySQL_keycloak_container ():
+    preDB=MySqlContainer('mysql:latest',MYSQL_USER="keycloak",MYSQL_PASSWORD="secret_password",MYSQL_DATABASE="keycloak",MYSQL_ROOT_PASSWORD= "secret_root_password")
+    preDB.with_volume_mapping("/home/ben/kc_weskit/wesnake/kc_login/test.sql","/docker-entrypoint-initdb.d/test.sql")
+    with  preDB as mysql:
+        print(yaml.dump(getContainerProperties(mysql,'3306')))
+        yield mysql
+        print("Killing keycloaks MySQL")
 
-    with DockerCompose("./kc_login","docker-compose.yaml") as kc_container:
-        kc_container.start()
-        url="http://%s:%s/auth/realms/WESkit/.well-known/openid-configuration" % (
-        kc_container.get_service_host("keycloak",8080),
-        kc_container.get_service_port("keycloak",8080))
-        kc_container.wait_for(url)
-        yield kc_container
+
+
+@pytest.fixture(scope="session")
+def keycloak_container(MySQL_keycloak_container):
+    mysqlIP=getContainerProperties(MySQL_keycloak_container,'3306')["InternalIP"]
+
+    kc_container=DockerContainer("jboss/keycloak")
+    kc_container.with_exposed_ports('8080')
+    kc_container.with_env("DB_VENDOR","mysql")
+    kc_container.with_env("DB_PORT",'3306')
+    kc_container.with_env("DB_ADDR", mysqlIP)
+    kc_container.with_env("DB_USER","keycloak")
+    kc_container.with_env("DB_PASSWORD","secret_password")
+    kc_container.start()
+    time.sleep(5)
+    kc_port=kc_container.get_exposed_port('8080')
+    kc_host=kc_container.get_container_host_ip()
+    print("Waiting for Keycloak Container")
+    print(yaml.dump(getContainerProperties(kc_container,'8080')))
+    retry=20
+    waitingSeconds=5
+    kc_running=False
+    for i in range(retry):
+        try:
+            print("connect to keycloak try : %d"%(i+1))
+            requests.get("http://"+kc_host+":"+kc_port)
+            kc_running=True
+            print("connection attempted try %d: Success"%(i+1))
+            break
+        except Exception:
+            print("connection attemped try %d: Failed - Will retry in %d seconds"%(i+1,waitingSeconds))
+            time.sleep(waitingSeconds)
+    assert kc_running
+    print("KC ready")
+    yield kc_container
+    print("Killing KeyCloak")
     
 @pytest.fixture(scope="session")
 def test_config():
