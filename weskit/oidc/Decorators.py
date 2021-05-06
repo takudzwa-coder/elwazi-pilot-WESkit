6ad4002b095fcd808b2bc9d150c109ec451837be
Philip R. Kensche <p.kensche@dkfz.de>
2021-05-06 09:29:28 +0200

    Moved functions form weskit/oidc/utils.py into Decorators.py. Added .coverage file to ignore lists.

--- weskit/oidc/Decorators.py
+++ weskit/oidc/Decorators.py
@@ -1,10 +1,13 @@
+import logging
 from functools import wraps
-from typing import Callable
+from typing import Callable, Optional
 
+import requests
 from flask import current_app
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended.view_decorators import _decode_jwt_from_headers
 
-from weskit.oidc.utils import online_validation
+logger = logging.getLogger(__name__)
 
 
 def login_required(fn: Callable,
@@ -42,3 +45,43 @@ def login_required(fn: Callable,
         return fn(*args, **kwargs)
 
     return wrapper
+
+
+def online_validation(app) -> bool:
+    """
+    Checks the validity of a token with a request to OIDC provider.
+    Returns True if cookie is valid and false if session ended.
+    """
+    access_token = get_token()
+    payload = {
+        "client_id": app.oidc_login.client_id,
+        "client_secret": app.oidc_login.client_secret,
+        "token": access_token
+    }
+    try:
+        j = requests.post(
+            data=payload,
+            url=app.oidc_login.introspection_endpoint
+        ).json()
+
+    except Exception as e:
+        logger.exception("Could not reach OIDC provider for online validation")
+        logger.exception(e)
+        return False
+
+    return j.get('active', False)
+
+
+def get_token(token_type: str = "access") -> Optional[str]:
+    """
+    This function returns the encoded access_token from different sources.
+    Its basically identical to a function from jwt_extended but has different
+    return
+    """
+
+    try:
+        encoded_token, csrf_token = _decode_jwt_from_headers()
+    except Exception:  # TODO Check: Can this be InvalidHeaderError?
+        # If Token is not in header
+        encoded_token = None
+    return encoded_token
