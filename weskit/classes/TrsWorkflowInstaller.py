065cbf466609fab4a5afbcc2444d387e0e6349e5
Philip R. Kensche <p.kensche@dkfz.de>
2021-11-11 19:23:10 +0100

    flufl.lock for locking

--- weskit/classes/TrsWorkflowInstaller.py
+++ weskit/classes/TrsWorkflowInstaller.py
@@ -5,16 +5,22 @@
 #      https://gitlab.com/one-touch-pipeline/weskit/api/-/blob/master/LICENSE
 #
 #  Authors: The WESkit Team
+import logging
 import os
+import random
 from dataclasses import dataclass
+from datetime import timedelta
 from pathlib import Path
 from typing import TypeVar, Optional
 from urllib.parse import urlparse
 
+from flufl.lock import Lock
 from trs_cli.client import TRSClient
 from trs_cli.models import FileType
 
 from weskit.ClientError import ClientError
+
+logger = logging.Logger(__name__)
 
 # Map the TRS types to WES workflow_types.
 trs_type_to_workflow_type = {
@@ -131,62 +137,62 @@ class TrsWorkflowInstaller:
     Retrieve the workflow from a trusted tool repository service (TRS).
     """
 
-    def __init__(self, trs_client: TRSClient, workflow_base_dir: Path):
+    def __init__(self,
+                 trs_client: TRSClient,
+                 workflow_base_dir: Path,
+                 max_lock_time: timedelta = timedelta(minutes=3)):
         self._workflow_base_dir = workflow_base_dir
         self._trs_client = trs_client
+        self._max_lock_time: timedelta = max_lock_time
 
-    def get_workflow_directory(self, workflow_info: WorkflowInfo) -> Path:
+    def _lockfile_name(self, workflow_dir: Path) -> Path:
+        return workflow_dir.parent / (workflow_dir.name + ".lock")
+
+    def _get_workflow_directory(self, workflow_info: WorkflowInfo) -> Path:
         return self._workflow_base_dir / \
                workflow_info.name / \
                workflow_info.version / \
                workflow_info.workflow_type
 
-    def workflow_is_installed(self, workflow_info: WorkflowInfo) -> bool:
-        # TODO This does not tell anything about whether a workflow installation is currently
-        #      ongoing. Concurrent workflow installations will wrongly assume the workflow
-        #      exists completely installed.
-        return self.get_workflow_directory(workflow_info).exists()
-
-    def _create_locked_workflow_directory(self, workflow_info: WorkflowInfo) -> Path:
-        # TODO This might raise an error, if e.g. two workflow installations run in parallel, but
-        #      at least thus it will prevent installation over an existing installation and
-        #      the resulting corruption. Depends on whether dir creation is atomic in Python.
-        workflow_dir = self.get_workflow_directory(workflow_info)
-        (workflow_dir / ".lock").mkdir(parents=True, exist_ok=False)
-        return workflow_dir
-
-    def _release_workflow_directory(self, workflow_info: WorkflowInfo):
-        (self.get_workflow_directory(workflow_info) / ".lock").rmdir()
+    def _workflow_is_installed(self, workflow_info: WorkflowInfo) -> bool:
+        return self._get_workflow_directory(workflow_info).exists()
 
     def _install_workflow(self, workflow_info: WorkflowInfo):
-        workflow_dir = self._create_locked_workflow_directory(workflow_info)
-        files_by_type = self._trs_client.retrieve_files(
-            out_dir=workflow_dir,
-            type=workflow_info.type,
-            id=workflow_info.name,
-            version_id=workflow_info.version,
-            is_encoded=False)   # TODO To decode or not to decode, that's the question!
+        try:
+            workflow_dir = self._get_workflow_directory(workflow_info)
+            workflow_dir.mkdir(parents=True, exist_ok=False)
+            files_by_type = self._trs_client.retrieve_files(
+                out_dir=workflow_dir,
+                type=workflow_info.type,
+                id=workflow_info.name,
+                version_id=workflow_info.version,
+                is_encoded=False)   # TODO To decode or not to decode, that's the question!
 
-        if FileType.PRIMARY_DESCRIPTOR.value in files_by_type.keys():
-            primary_files = files_by_type[FileType.PRIMARY_DESCRIPTOR.value]
-            if len(primary_files) != 1:
-                raise ClientError(f"Multiple primary files in '{workflow_info.full_uri}'. " +
-                                  "Choose one of %s" % ", ".join(primary_files))
-            elif workflow_info.primary_file is not None:
-                primary_file_rel = workflow_info.primary_file
-            else:
-                primary_file_rel = primary_files[0]
+            if FileType.PRIMARY_DESCRIPTOR.value in files_by_type.keys():
+                primary_files = files_by_type[FileType.PRIMARY_DESCRIPTOR.value]
+                if len(primary_files) != 1:
+                    raise ClientError(f"Multiple primary files in '{workflow_info.full_uri}'. " +
+                                      "Choose one of %s" % ", ".join(primary_files))
+                elif workflow_info.primary_file is not None:
+                    primary_file_rel = workflow_info.primary_file
+                else:
+                    primary_file_rel = primary_files[0]
 
-            primary_file_abs = workflow_dir / primary_file_rel
-            if not os.access(primary_file_abs):
-                # Installation-time check, that the primary file really was downloaded.
-                raise RuntimeError(f"Could not access primary workflow file: '{primary_file_abs}")
+                primary_file_abs = workflow_dir / primary_file_rel
+                if not os.access(primary_file_abs):
+                    # Installation-time check, that the primary file really was downloaded.
+                    # It's more likely a client error than an NFS error, therefore report is as
+                    # such.
+                    raise ClientError("Could not access primary workflow file: " +
+                                      f"'{primary_file_abs}'")
 
-        elif FileType.CONTAINERFILE in files_by_type.keys():
-            raise NotImplementedError(
-                "Cannot process container-based TRS workflows yet")
-
-        self._release_workflow_directory(workflow_info)
+            elif FileType.CONTAINERFILE in files_by_type.keys():
+                raise NotImplementedError(
+                    "Cannot process container-based TRS workflows yet")
+        except OSError as ex:
+            message = f"Failed installing TRS workflow from '{workflow_info.full_uri}'"
+            logger.warning(message, exc_info=ex)
+            raise ClientError(message)
 
     def install(self, workflow_info: WorkflowInfo) -> WorkflowMetadata:
         """
@@ -194,10 +200,29 @@ class TrsWorkflowInstaller:
         path to workflow main file (e.g. main.nf, Snakefile), conda environment for the workflow
         (e.g. for supplementary libraries), workflow manager version, etc.
         """
-        if not self.workflow_is_installed(workflow_info):
-            self._install_workflow(workflow_info)
+        workflow_dir_abs = self._get_workflow_directory(workflow_info)
+        try:
+            with Lock(lockfile=str(self._lockfile_name(workflow_dir_abs)),
+                      lifetime=self._max_lock_time,
+                      timeout=self._max_lock_time +
+                      timedelta(seconds=random.randint(1, 30))):  # nosec b311
+                # We always acquire a lock, when a workflow is requested. If it is already
+                # installed, the lock will directly be released. Otherwise, one process acquires
+                # the lock (even via NFS!) and the others wait till the workflow is installed. The
+                # others, will either find the workflow successfully installed or retry the
+                # installation, possibly with the same error. We leave it to the client not to
+                # overload WESkit with senseless installation tries.
+                #
+                # The random timeout offset is to give the processes time to check whether the
+                # directory exists.
+                if not self._workflow_is_installed(workflow_info):
+                    self._install_workflow(workflow_info)
+        except TimeoutError:
+            message = "Timed out when waiting for concurrent TRS workflow installations of " + \
+                f"'{workflow_info.full_uri}'"
+            logger.warning(message)
+            raise ClientError(message)
 
-        workflow_dir_rel = self.get_workflow_directory(workflow_info).\
-            relative_to(self._workflow_base_dir)
+        workflow_dir_rel = workflow_dir_abs.relative_to(self._workflow_base_dir)
         return WorkflowMetadata(workflow_dir=workflow_dir_rel,
                                 workflow_info=workflow_info)
