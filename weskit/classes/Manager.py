ed6f9cdc886bc08cf6c1fc9d5a242bb01d93c478
schneiva <valentin.schneider-lunitz@charite.de>
2022-05-12 16:04:16 +0200

    correct intendation

--- weskit/classes/Manager.py
+++ weskit/classes/Manager.py
@@ -162,7 +162,8 @@ class Manager:
             -> Optional[Run]:
         if not isinstance(request["workflow_params"], dict):
             request["workflow_params"] = json.loads(request["workflow_params"])
-            request["workflow_engine_parameters"] = json.loads(request["workflow_engine_parameters"])
+            request["workflow_engine_parameters"] = \
+                json.loads(request["workflow_engine_parameters"])
         run = Run(data={"run_id": self.database.create_run_id(),
                         "run_status": RunStatus.INITIALIZING.name,
                         "request_time": get_current_timestamp(),
