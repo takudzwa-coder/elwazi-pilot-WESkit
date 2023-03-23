d131eed1375bb42c3b08307c9ee5cc9748967866
Valentin Schneider-Lunitz <valentin.schneider-lunitz@charite.de>
2023-03-23 23:07:31 +0100

    keep processing stage if terminal

--- weskit/classes/ProcessingStage.py
+++ weskit/classes/ProcessingStage.py
@@ -175,6 +175,8 @@ class ProcessingStage(enum.Enum):
         """
         if self.allowed_to_progress_to(new_state):
             return new_state
+        elif self.is_terminal:
+            return self
         else:
             raise RuntimeError(
                 f"Forbidden state-change requested: '{self.name}' -/-> '{new_state.name}'")
