b911ff01ed740a467066e884a4cf2fa7c7365b76
Takudzwa Musarurwa <tnmusarurwa@gmail.com>
2024-04-15 15:16:15 +0200

    s3 url-WIP

--- weskit/api/DrsUrlResolver.py
+++ weskit/api/DrsUrlResolver.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import json
 import logging
 import os
+import socket
 import re
 import requests
 from copy import deepcopy
@@ -45,17 +46,22 @@ def drsUrlResolver(data):
 
 
                 drs_obj = requests.request(http_method, request_url)
-                if drs_hostname in ['localhost', '127.0.0.1']:
-                    access_methods = drs_obj.json()["access_methods"][0]
-                    file_path = access_methods["access_url"]["url"]
-                    input_objs.append(file_path)
+                if drs_hostname in ['localhost', '127.0.0.1', socket.gethostbyname(socket.gethostname())]:
+                    access_methods = drs_obj.json()["access_methods"]
+                    access_url = access_methods[0]["access_url"]["url"]
+                    input_objs.append(access_url)
 
                 else:
-                    access_methods = drs_obj.json()["access_methods"][1]
-                    file_access_id = access_methods["access_id"]
-                    drs_streaming_path = "stream"
-                    drs_streaming_url = ga4gh_drs_base_url.format(drs_port,"drs", drs_streaming_path) + drs_id + "/" +file_access_id
-                    input_objs.append(drs_streaming_url)
+                    access_methods = drs_obj.json()["access_methods"]
+                    if access_methods[0]["type"] == 's3':
+                        access_url = access_methods[0]["access_url"]["url"]
+                        input_objs.append(access_url)
+                    else:
+                        print(access_methods)
+                        file_access_id = access_methods[1]["access_id"]
+                        drs_streaming_url = ga4gh_drs_base_url.format(drs_port,"drs", "stream") + drs_id + "/" +file_access_id
+                        input_objs.append(drs_streaming_url)
+                    
         parse_result[param] = " ".join(input_objs)
     data["workflow_params"] = json.dumps(parse_result)
     return data
